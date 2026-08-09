722dce55a7a00d6018ada51c1fc33644bc3e00e0
kammu2000 <deepanshuupadhyay1310@gmail.com>
2026-08-09 06:33:10 +0530

    [CHESS-ANALYZER] chore: added cmake formatter + mise and formatted files

---
+++ .cmake-format.py
@@ -0,0 +1,25 @@
+# Formatting
+line_width = 80
+tab_size = 2
+use_tabchars = False
+
+# Layout
+max_pargs_hwrap = 4
+max_subgroups_hwrap = 2
+max_rows_cmdline = 2
+
+# Spacing
+separate_ctrl_name_with_space = True
+separate_fn_name_with_space = False
+dangle_parens = True
+dangle_align = "prefix"
+
+# Comments
+bullet_char = "*"
+first_comment_is_literal = False
+literal_comment_pattern = None
+
+# Sorting
+enable_sort = True
+autosort = True
+
